54625fa6242c23cf5884e00ba6f28f0cceb90f0b
jpulattie <jpulattie@gmail.com>
2023-10-19 07:31:02 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Description:  Program converts pdf files in a given directory, converts them to png files, and deletes the pdfs
 
 from pdf2image import convert_from_path
+from pathlib import Path
 import os
 import re
 
@@ -21,16 +22,21 @@ def strip_number(name, count):
 for i in range(len(files)):
     pdf = files[i]
     file_name = os.path.basename(pdf)
-    recur_name = strip_number(file_name, len(file_name)-4)
-    shortened2 = re.sub(r'Quote', '', recur_name)
-    shortened3 = re.sub(r'1', '', shortened2)                   # clean up file name
-    new_pic = convert_from_path(pdf_directory+"/"+pdf, output_file=shortened3, output_folder=pic_directory, fmt=".PNG")
 
+    extension = Path(file_name).suffix
+
+    if extension == ".pdf":
+        recur_name = strip_number(file_name, len(file_name)-4)
+        shortened2 = re.sub(r'Quote', '', recur_name)
+        shortened3 = re.sub(r'1', '', shortened2)# clean up file name
+        print(shortened3)
+        new_pic = convert_from_path(pdf_directory+"/"+pdf, output_file=shortened3, output_folder=pic_directory, fmt=".PNG")
 
 for i in range(len(files)):
     pdf = files[i]
     file_name = os.path.basename(pdf)
     if file_name in files:
-        delete = os.remove(pdf_directory + "/" + file_name)     # deletes pdf files when job is complete
+        print("1")
+        #delete = os.remove(pdf_directory + "/" + file_name)     # deletes pdf files when job is complete
 
 print("finished")
